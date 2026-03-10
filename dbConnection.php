bfe55a01273723f7be1f91b11cb5747807037bd4
Subhasis sahoo <126506594+sahoo-subhasis@users.noreply.github.com>
2026-03-10 16:24:30 +0530

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -2,7 +2,8 @@
 $databaseHost = 'mysql-service';
 $databaseName = 'test';
 $databaseUsername = 'root';
-$databasePassword = 'root';
+$databasePassword = 'root123';
 
 // Open a new connection to the MySQL server
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
