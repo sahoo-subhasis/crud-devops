9addfe76432a9e48fdd1c56039372c617ee2e01d
Subhasis sahoo <126506594+sahoo-subhasis@users.noreply.github.com>
2026-03-10 16:32:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // Include the database connection file
 require_once("dbConnection.php");
 
-// Fetch data in descending order (lastest entry first)
+// Fetch data in descending order (latest entry first)
 $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
 ?>
 
@@ -12,28 +12,35 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
 </head>
 
 <body>
-	<h2>Homepage</h2>
-	<p>
-		<a href="add.php">Add New Data</a>
-	</p>
-	<table width='80%' border=0>
-		<tr bgcolor='#DDDDDD'>
-			<td><strong>Name</strong></td>
-			<td><strong>Age</strong></td>
-			<td><strong>Email</strong></td>
-			<td><strong>Action</strong></td>
-		</tr>
-		<?php
-		// Fetch the next row of a result set as an associative array
-		while ($res = mysqli_fetch_assoc($result)) {
-			echo "<tr>";
-			echo "<td>".$res['name']."</td>";
-			echo "<td>".$res['age']."</td>";
-			echo "<td>".$res['email']."</td>";	
-			echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | 
-			<a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
-		}
-		?>
-	</table>
+
+<h3 style="color:green;">Deployed from GitHub via Jenkins CI/CD 🚀</h3>
+
+<h2>Homepage</h2>
+<p>
+	<a href="add.php">Add New Data</a>
+</p>
+
+<table width='80%' border=0>
+<tr bgcolor='#DDDDDD'>
+<td><strong>Name</strong></td>
+<td><strong>Age</strong></td>
+<td><strong>Email</strong></td>
+<td><strong>Action</strong></td>
+</tr>
+
+<?php
+while ($res = mysqli_fetch_assoc($result)) {
+	echo "<tr>";
+	echo "<td>".$res['name']."</td>";
+	echo "<td>".$res['age']."</td>";
+	echo "<td>".$res['email']."</td>";	
+	echo "<td>
+	<a href=\"edit.php?id=$res[id]\">Edit</a> | 
+	<a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a>
+	</td>";
+}
+?>
+
+</table>
 </body>
 </html>
